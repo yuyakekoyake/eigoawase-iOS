a957ed9e10536b4c3ad5cedd28f6cd6fdb7a9b2c
y.okoshi <yuyakekoyakeno0430@gmail.com>
2017-12-12 23:29:15 +0900

    ver.0.1.0

---
+++ eigo_awase/CardControl.swift
@@ -0,0 +1,145 @@
+//
+//  CardControl.swift
+//  eigo_awase
+//
+//  Created by y.okoshi on 2017/12/12.
+//  Copyright © 2017年 english-society. All rights reserved.
+//
+
+import UIKit
+
+protocol CardControlDelegate: class {
+    func OnCardTapped(sender: CardControl)
+}
+
+class CardControl: UIView {
+    
+    // MARK: プロパティ
+    var number:Int = 0 {
+        didSet {
+            buttonCard.setTitle(String(number), for: .selected)
+        }
+    }
+    var isOpen = false {
+        didSet {
+            buttonCard.isSelected = isOpen
+            if isOpen {
+                buttonCard.backgroundColor = UIColor(red: 0.2, green: 0.8, blue: 0.8, alpha: 0.9)
+            }
+            else {
+                buttonCard.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 0.6, alpha: 0.7)
+            }
+        }
+    }
+    
+    weak var delegate: CardControlDelegate?
+    
+    static let sizeCard = 70
+    
+    var buttonCard = UIButton()
+    
+    // MARK: 初期化
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        buttonCard.setTitle("?", for: .normal)
+        buttonCard.layer.cornerRadius = 10
+        buttonCard.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 0.6, alpha: 0.7)
+        buttonCard.addTarget(self, action: #selector(CardControl.ButtonTapped(sender:)),for: .touchUpInside)
+    
+        addSubview(buttonCard)
+    }
+    
+    init(number: Int, frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.number = number
+        let buttonImage = UIImage(named:"saru_original_透過.png")
+        let buttonImageSelect = UIImage(named:"buttonBack.jpg")
+        //buttonCard.setTitle("⭐︎", for: .normal)
+         buttonCard.setBackgroundImage(buttonImage, for: .normal)
+        
+        var cardstr = ""
+        
+        switch number {
+        case 0:
+            cardstr = "apple"
+        case 1:
+            cardstr = "orange"
+        case 2:
+            cardstr = "cherry"
+        case 3:
+            cardstr = "banana"
+        case 4:
+            cardstr = "grape"
+        case 5:
+            cardstr = "lemon"
+        case 6:
+            cardstr = "peach"
+        case 7:
+            cardstr = "melon"
+        case 8:
+            cardstr = "りんご"
+        case 9:
+            cardstr = "オレンジ"
+        case 10:
+            cardstr = "さくらんぼ"
+        case 11:
+            cardstr = "ばなな"
+        case 12:
+            cardstr = "ぶどう"
+        case 13:
+            cardstr = "れもん"
+        case 14:
+            cardstr = "もも"
+        case 15:
+            cardstr = "めろん"
+        default:
+            cardstr = "test"
+        }
+        buttonCard.setTitleColor(UIColor.black, for: .selected)
+        buttonCard.setBackgroundImage(buttonImageSelect, for: .selected)
+        buttonCard.setTitle(cardstr, for: .selected)
+        buttonCard.layer.cornerRadius = 10
+        buttonCard.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 0.6, alpha: 0.7)
+        buttonCard.addTarget(self, action: #selector(CardControl.ButtonTapped(sender:)),for: .touchUpInside)
+        
+        addSubview(buttonCard)
+    }
+    
+    override open var intrinsicContentSize: CGSize {
+        //コンテンツサイズの設定
+        return CGSize(width: CardControl.sizeCard, height: CardControl.sizeCard)
+    }
+    
+    override func layoutSubviews() {
+        //ボタンサイズの設定
+        buttonCard.frame = CGRect(x: 0, y: 0, width: CardControl.sizeCard, height: CardControl.sizeCard)
+    }
+    
+    // MARK: ボタンのタッチイベント
+    func ButtonTapped(sender: UIButton) {
+        delegate?.OnCardTapped(sender: self)
+    }
+    
+    // MARK: メソッド
+    func OpenCard() -> Int? {
+        if isOpen {
+            return nil
+        }
+        else {
+            isOpen = true
+            return number
+        }
+    }
+    
+    func CloseCard() {
+        isOpen = false
+    }
+    
+    func DisableCard() {
+        buttonCard.removeTarget(self, action: #selector(CardControl.ButtonTapped(sender:)),for: .touchUpInside)
+        buttonCard.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 0.6, alpha: 0.3)
+    }
+}
+
--- eigo_awase/ViewController.swift
+++ eigo_awase/ViewController.swift
@@ -8,18 +8,156 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CardControlDelegate {
 
+    var cards = [CardControl]()
+    var cardSelected: CardControl?
+    var canOpen = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var numArray = [Int]()
+        for i in 0..<16 {
+            numArray.append(i)
+        }
+        print(numArray)
+        //var numArray: [String] = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        
+        for i in 0..<16 {
+            let idx = Int(arc4random_uniform(UInt32(numArray.count)))
+            let card = CardControl(number: numArray[idx], frame: CGRect(x: 30 + (CardControl.sizeCard + 10) * (i % 4), y: 30 + (CardControl.sizeCard + 10) * (i / 4), width: CardControl.sizeCard, height: CardControl.sizeCard))
+            card.delegate = self
+            view.addSubview(card)
+            cards.append(card)
+            numArray.remove(at: idx)
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: Card Action
+    func OnCardTapped(sender: CardControl) {
+        if !canOpen {
+            print("カードは現在めくれません")
+            return
+        }
+        if sender.isOpen {
+            print("カードは既に開いています")
+        }
+        else {
+            let number = sender.OpenCard()!
+            var AsrNum = 0
+            switch number {
+            case 0:
+                AsrNum = 0
+            case 1:
+                AsrNum = 1
+            case 2:
+                AsrNum = 2
+            case 3:
+                AsrNum = 3
+            case 4:
+                AsrNum = 4
+            case 5:
+                AsrNum = 5
+            case 6:
+                AsrNum = 6
+            case 7:
+                AsrNum = 7
+            case 8:
+                AsrNum = 0
+            case 9:
+                AsrNum = 1
+            case 10:
+                AsrNum = 2
+            case 11:
+                AsrNum = 3
+            case 12:
+                AsrNum = 4
+            case 13:
+                AsrNum = 5
+            case 14:
+                AsrNum = 6
+            case 15:
+                AsrNum = 7
+            default:
+                print("Asrnumエラー")
+            }
+            
+            if cardSelected == nil {
+                cardSelected = sender
+                print("1枚目のカードを開きました．: \(number)")
+            }
+            else {
+                print("2枚目のカードを開きました．: \(number)")
+                var AsrNum1 = 0
+                switch cardSelected!.number {
+                case 0:
+                    AsrNum1 = 0
+                case 1:
+                    AsrNum1 = 1
+                case 2:
+                    AsrNum1 = 2
+                case 3:
+                    AsrNum1 = 3
+                case 4:
+                    AsrNum1 = 4
+                case 5:
+                    AsrNum1 = 5
+                case 6:
+                    AsrNum1 = 6
+                case 7:
+                    AsrNum1 = 7
+                case 8:
+                    AsrNum1 = 0
+                case 9:
+                    AsrNum1 = 1
+                case 10:
+                    AsrNum1 = 2
+                case 11:
+                    AsrNum1 = 3
+                case 12:
+                    AsrNum1 = 4
+                case 13:
+                    AsrNum1 = 5
+                case 14:
+                    AsrNum1 = 6
+                case 15:
+                    AsrNum1 = 7
+                default:
+                    print("Asrnum1エラー")
+                }
+                if AsrNum1 == AsrNum{
+                    print("成功！")
+                    cardSelected?.DisableCard()
+                    sender.DisableCard()
+                    cardSelected = nil
+                    for (_, obj) in cards.enumerated() {
+                        if !obj.isOpen {
+                            return
+                        }
+                    }
+                    print("ALL CLEAR!!")
+                }
+                else {
+                    print("失敗．．")
+                    self.canOpen = false
+                    DispatchQueue.global(qos: .userInitiated).async {
+                        sleep(1)
+                        DispatchQueue.main.async {
+                            self.cardSelected?.CloseCard()
+                            sender.CloseCard()
+                            self.cardSelected = nil
+                            self.canOpen = true
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
